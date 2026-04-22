a3b96359bf94a69d8a388144f08dd32774443bb2
codebell-alt <isabella315784@gmail.com>
2026-04-22 09:34:51 -0500

    fix: Remover nodeVersion de vercel.ts

--- vercel.ts
+++ vercel.ts
@@ -3,6 +3,5 @@ export default {
   buildCommand: 'cd Frontend && pnpm build',
   devCommand: 'cd Frontend && pnpm dev',
   installCommand: 'cd Frontend && pnpm install',
-  outputDirectory: 'Frontend/.next',
-  nodeVersion: '20.x'
+  outputDirectory: 'Frontend/.next'
 };
